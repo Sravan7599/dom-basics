2956f853f946510f87a42d24ea133cb5065e354f
Sravan7599 <kcmreddykummitha@gmail.com>
2021-08-15 11:48:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+document
+console.log(document);
+document.title
+document.title="basics-dom";
+console.log(document.title);
+
+let heading= document.getElementById("access101");
+console.log(heading);
+
+let heading=document.getElementsByTagName("h1");
+console.log(heading);
+
+
+let heading= document.getElementsByClassName("para");
+console.log(heading);
+
+let heading=document.querySelector("h1");
+console.log(heading);
+
+let heading=document.querySelectorAll(.access101);
+console.log(heading);
+
+
