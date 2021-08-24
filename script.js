484d9d860e07c75e5d76f3bbbe287033771254d5
Sravan7599 <kcmreddykummitha@gmail.com>
2021-08-24 15:11:35 +0530

    first commit

--- script.js
+++ script.js
@@ -4,20 +4,48 @@ document.title
 document.title="basics-dom";
 console.log(document.title);
 
+
+// getElementById
+// getElementsByTagName
+// getElementsByClassName
+// queryselector
+// queryselector all
+
+
+
+// getElementById
 let heading= document.getElementById("access101");
 console.log(heading);
 
+// getElementsByTagName
 let heading=document.getElementsByTagName("h1");
 console.log(heading);
 
-
+// getElementsByClassName
 let heading= document.getElementsByClassName("para");
 console.log(heading);
 
+// queryselector
 let heading=document.querySelector("h1");
 console.log(heading);
 
-let heading=document.querySelectorAll(.access101);
+let heading=document.querySelector(".para");
 console.log(heading);
+
+let heading=document.querySelector("#access101");
+console.log(heading);
+// limitation of query selector is when there are more number of same tags then it access only the first tag, it wont access the remaining tags
+let heading=document.querySelector("p");
+console.log(heading);
+
+
+// queryselector all
+let heading=document.querySelectorAll(".access101");
+console.log(heading);
+
+// use query selector all, it access all the elements or tags but queryselector access only the first element
+let heading=document.querySelectorAll("p");
+console.log(heading);
+
 
 
